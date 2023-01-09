fd098073e6bcdd9f02e166798832fcf4be755830
s.tittel <s.tittel@ecentral.de>
2023-01-09 23:10:23 +0100

    Install prismjs for code highlightning andupdate example,js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Example from "./components/Example";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>fun-box</h1>
-          <p>Dieses Repo, enthält Codebeispiele. Die Beispiele sind in verschiedenen Branches abgelegt, sodass nach auschecken dieser die Beispiele ausgeführt werden können. Folgen  sie hierfür der Anleitung in der README.md im Repo...</p>
-          <Example />
+    <div className="bg-gray-800  text-gray-200 p-4 rounded-md">
+      <header className="text-center text-2xl font-bold">
+        <h1 className="text-yellow-500">fun-box</h1>
+          <p className="text-base">Dieses Repo, enthält Codebeispiele. Die Beispiele sind in verschiedenen Branches abgelegt, sodass nach auschecken dieser die Beispiele ausgeführt werden können. Folgen  sie hierfür der Anleitung in der README.md im Repo...</p>
       </header>
+      <Example />
     </div>
   );
 }
--- src/components/Example.js
+++ src/components/Example.js
@@ -1,20 +1,27 @@
+import Prism from 'prismjs';
 import Solution from './Solution';
+import {useEffect} from "react";
 
 // Die Aufgaben sind so zu lösen, das über die nutzung der Komponenten die Aufgabe ausgegeben werden kann
 // und die Lösung dargestellt wird. In der Example wird die Aufgabe erstellt und alle Informationen hierzu mit eingebunden.
 // Die Lösung wird in diser als Komponente mit eingebunden.
+const code = `const Example = (...props) => {true};`;
 const Example = () => {
+	useEffect(() => {
+		Prism.highlightAll();
+	}, []);
 	return (
 		<>
-			<div>
+			<div className="py-8 text-center">
 				<h2>Name der Aufgabe</h2>
 				<p>Beschreibung der aufgabe</p>
-				<pre className="bg-gray-800 text-gray-200 p-4 rounded-md">
-					<code className="text-xs">
-						Hier wird der Code der Aufgabe dargestellt
-					</code>
-				</pre>
-				<code>Evt Code beispiele zur Aufgabe</code>
+				<div className="mx-auto my-auto flex justify-center items-center">
+					<pre>
+						<code className="language-js">
+							{code}
+						</code>
+					</pre>
+				</div>
 			</div>
 			<Solution name="World"/>
 		</>
