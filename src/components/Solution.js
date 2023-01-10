b6b689dc225ae89984c4a741f478eea0858a2036
s.tittel <s.tittel@ecentral.de>
2023-01-10 19:55:24 +0100

    Resolve Codebox Bug

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -5,10 +5,10 @@ const Solution = () => {
 	const data = {
 		date: 'Jan 9, 2023',
 		topic: 'JavaScript',
-		title: 'Name der Aufgabe',
+		title: 'Lösungsweg',
 		description: 'Beschreibung der Aufgabe!',
-		code: `const Example = (...props) => {true};`,
-		link: '#'
+		code: `const Solution = (...props) => {true};`,
+		link: 'Link to Codesandbox'
 	}
 
 	return (
